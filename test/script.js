4fd6408371c33881cb0b1cb7a524137c6978fb71
effwe <36579899+effwe@users.noreply.github.com>
2022-02-28 09:24:16 +0800

    Add files via upload

--- test/script.js
+++ test/script.js
@@ -4,8 +4,14 @@ window.addEventListener('scroll', () => {
     const Y = window.scrollY;
     const innerText = document.querySelector('.innerText');
     const Text = document.querySelector('.Text');
+    const anc = document.querySelector('.anc');
 
-    innerText.style.left = 100 - Y / 5 + '%';
-    Text.style.left = 100 - Y / 5 + '%';
+    innerText.style.left = -Y / 5 + '%';
+    Text.style.left = -Y / 5 + '%';
+
+    anc.style.top = 15 + -Y/10 + '%';
+    innerText.style.top = 15 + -Y / 10 + '%';
+    Text.style.top = 15 + -Y / 10 + '%';
+
   });
 
